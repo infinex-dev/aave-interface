5c6b3aaca7c8fc5f73ed7698712ec15876bd8b37
min-infinex <min@core.infinex.xyz>
2025-06-24 17:02:13 -0400

    remove unused routes

--- src/components/primitives/Link.tsx
+++ src/components/primitives/Link.tsx
@@ -123,16 +123,16 @@ export const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(function Link
 export const ROUTES = {
   dashboard: '/',
   markets: '/markets',
-  staking: '/staking',
-  governance: '/governance',
-  faucet: '/faucet',
-  migrationTool: '/v3-migration',
-  marketMigrationTool: (marketName: CustomMarket) => `/v3-migration/?market=${marketName}`,
-  dynamicRenderedProposal: (proposalId: number) =>
-    `/governance/v3/proposal?proposalId=${proposalId}`,
+  // staking: '/staking',
+  // governance: '/governance',
+  // faucet: '/faucet',
+  // migrationTool: '/v3-migration',
+  // marketMigrationTool: (marketName: CustomMarket) => `/v3-migration/?market=${marketName}`,
+  // dynamicRenderedProposal: (proposalId: number) =>
+  // `/governance/v3/proposal?proposalId=${proposalId}`,
   reserveOverview: (underlyingAsset: string, marketName: CustomMarket) =>
     `/reserve-overview/?underlyingAsset=${underlyingAsset}&marketName=${marketName}`,
-  history: '/history',
-  bridge: '/bridge',
-  safetyModule: '/safety-module',
+  // history: '/history',
+  // bridge: '/bridge',
+  // safetyModule: '/safety-module',
 };
